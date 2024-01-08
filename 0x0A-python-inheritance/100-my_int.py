5ba47d5d6b7ccec0e4d6f4285e60610b2fee0888
mohd33034914 <mohd33034914@gmail.com>
2024-01-08 22:44:27 +0000

    py;

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class that inherits int"""
+# 100-my_int.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """Redefine equality comparisons"""
-    def __eq__(self, other_int):
-        """Return equal if not equal"""
-        if self is not other_int:
-            return True
-        else:
-            return False
+    """Invert int operators == and !=."""
 
-    def __ne__(self, other_int):
-        """Return not equal if equal"""
-        if self is other_int:
-            return True
-        else:
-            return False
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
